846a177d4fb8a291e847237df5183d17126b0e2f
Sachin Chandwani <sachinchandwani@sachin.local>
2020-01-24 20:40:53 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,6 @@ import './App.css';
 import Navbar from './Navbar'
 import Doctorabi from '../abis/Doctor.json'
 import Patientabi from '../abis/Patient.json'
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> b2d173000e6f92bd2529f83583cc513cd2f18d09
->>>>>>> 735b4af18f25b9d0157b7a3f49a5a5e14a963e01
 
 
 class App extends Component {
@@ -21,37 +14,14 @@ class App extends Component {
   }
 
 async loadBlockchainData(){
-<<<<<<< HEAD
   const web3 = new Web3(Web3.givenProvider)
-=======
-<<<<<<< HEAD
-  var net_id = 5777
-  const web3 = new Web3(Web3.givenProvider||  "http://localhost:7545")
-=======
-  const web3 = new Web3(Web3.givenProvider)
->>>>>>> b2d173000e6f92bd2529f83583cc513cd2f18d09
->>>>>>> 735b4af18f25b9d0157b7a3f49a5a5e14a963e01
   const accounts = await web3.eth.getAccounts()
   console.log(web3)
   
   this.setState({account:accounts[0]})  
-<<<<<<< HEAD
   var net_id = 5777
   // await web3.eth.net.getId()
   var ret=0    
-=======
-<<<<<<< HEAD
-  var ret=0 
-  if(this.state.account==undefined){
-    alert("Sign in to your blockchain wallet through metamask");
-  } 
-  else{  
-=======
-  var net_id = 5777
-  // await web3.eth.net.getId()
-  var ret=0    
->>>>>>> b2d173000e6f92bd2529f83583cc513cd2f18d09
->>>>>>> 735b4af18f25b9d0157b7a3f49a5a5e14a963e01
   if(Doctorabi.networks[net_id]){
     const d_abi= Doctorabi.abi
     var address= Doctorabi.networks[net_id].address
@@ -77,20 +47,9 @@ async loadBlockchainData(){
   this.setState({loading:false})
   }
 }
-<<<<<<< HEAD
 
  
   
-=======
-<<<<<<< HEAD
-}
-  // this.setState({account:web3})
-=======
-
- 
-  
->>>>>>> b2d173000e6f92bd2529f83583cc513cd2f18d09
->>>>>>> 735b4af18f25b9d0157b7a3f49a5a5e14a963e01
 }
 constructor(props){
   super(props)
@@ -109,20 +68,8 @@ constructor(props){
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto"> 
               {this.state.loading 
-<<<<<<< HEAD
                 ? <div id="loader" className="text-center"><h1 className="text-center">Loading..</h1></div>
                 : 
-=======
-<<<<<<< HEAD
-                ? <div id="loader" className="text-center"><h1 className="text-center">Loading<span id="wait">.</span>
-                </h1></div>
-                : 
-                
-=======
-                ? <div id="loader" className="text-center"><h1 className="text-center">Loading..</h1></div>
-                : 
->>>>>>> b2d173000e6f92bd2529f83583cc513cd2f18d09
->>>>>>> 735b4af18f25b9d0157b7a3f49a5a5e14a963e01
                 <div>
                     <a  href="/Patient"><button type="button" className="btn btn-primary" >Patient</button></a>
                     <a  href="/Doctor"><button type="button" className="btn btn-primary" >Doctor</button></a>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,15 +11,7 @@ class Navbar extends Component {
           target="_blank"
           rel="noopener noreferrer"
         ><img src="/logo.png" height="80" width="80"></img>
-<<<<<<< HEAD
             Healthcare in BLOCKCHAIN
-=======
-<<<<<<< HEAD
-          Healthcare in BLOCKCHAIN
-=======
-            Healthcare in BLOCKCHAIN
->>>>>>> b2d173000e6f92bd2529f83583cc513cd2f18d09
->>>>>>> 735b4af18f25b9d0157b7a3f49a5a5e14a963e01
         </div>
         
         <p className="text-white"> {this.props.account}</p>
