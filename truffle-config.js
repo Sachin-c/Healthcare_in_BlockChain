1c1c1945aad82053d966ac35648320f2e7c0a3f2
Sachin Chandwani <sachinchandwani@sachin.local>
2020-05-13 13:07:48 +0530

    ..

--- truffle-config.js
+++ truffle-config.js
@@ -1,24 +1,20 @@
 require('babel-register');
 require('babel-polyfill');
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-
 const mnemonic = "empower copper broccoli aim release pig foam mule ranch salmon brisk royal";
-
 module.exports = {
   networks: {
-  //   ropsten: {
-  //   provider: function() {
-  //     return new HDWalletProvider(mnemonic, "https://sandbox.truffleteams.com/025ee19b-5c67-4f4e-9a40-b88b5742528f");
-  //   },
-  //   network_id: '1589235947767',
-  // },
-
     development: {
-      // Hostname: "bit.com",
       provider: function() {
         return new HDWalletProvider(mnemonic, "https://sandbox.truffleteams.com/025ee19b-5c67-4f4e-9a40-b88b5742528f");
       }, 
-      // host: "https://sandbox.truffleteams.com/025ee19b-5c67-4f4e-9a40-b88b5742528f",
+      port: 443,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://sandbox.truffleteams.com/025ee19b-5c67-4f4e-9a40-b88b5742528f");
+      }, 
       port: 443,
       network_id: "*" // Match any network id
     },
